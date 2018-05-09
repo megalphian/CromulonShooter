c04dbb72b9c680e61ed052052cef7756511177a8
Megnath Ramesh <megnath@ualberta.ca>
2018-05-09 14:57:43 -0600

    Initial Commit

---
+++ Assets/Scripts/FpvControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FpvControl : MonoBehaviour
+{
+	private Vector3 cameraRotation;
+	private float x, y;
+
+
+    // Initializing Camera Position
+    void Start()
+    {
+		//cameraObject = Camera.main;
+		//cameraPosition = cameraObject.transform.localPosition;
+    }
+
+    void RotateCam(float x, float y)
+	{
+		cameraRotation.Set(x, -y, 0f);
+		transform.eulerAngles -= cameraRotation;
+	}
+
+    // FixedUpdate is called every time the arrow key is pressed
+    void FixedUpdate()
+    {
+		x = Input.GetAxis("Mouse Y");
+		y = Input.GetAxis("Mouse X");
+		Debug.Log("Current Axis" + x + " : " + y);
+
+		RotateCam(x, y);
+    }
+}
