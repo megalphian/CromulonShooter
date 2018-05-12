14f22abf30c36a6b2670b5da2fc61ae5aeeb17b6
Megnath Ramesh <megnath@ualberta.ca>
2018-05-12 15:13:19 -0600

    Locked cursor

--- Assets/Scripts/FpvControl.cs
+++ Assets/Scripts/FpvControl.cs
@@ -5,14 +5,19 @@ public class FpvControl : MonoBehaviour
 {
 	private Vector3 cameraRotation;
 	private float x, y;
-       
-    void RotateCam()
+
+	private void Start()
 	{
-		if (x > -90 && x < 90)
-		{
-			cameraRotation.Set(x, -y, 0f);
-			transform.eulerAngles -= cameraRotation;
-		}
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+
+	void RotateCam()
+	{
+		x = Mathf.Clamp(x, -90, 90);
+		cameraRotation.Set(x, -y, 0f);
+		transform.eulerAngles -= cameraRotation;
+
 	}
 
     // FixedUpdate is called every time the mouse is moved
